a7a0732d87e830dd5ee5052c203a11e24d6b7d7a
Dhiraj Kumar Mehta <dk266107@gmail.com>
2022-12-03 20:25:03 +0530

    Project Added

---
+++ Spring-Cloud-Github/src/main/java/com/cloud/github/SpringCloudGithubApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGithubApplication.class, args);
+	}
+
+}
---
+++ Spring-Cloud-Github/src/test/java/com/cloud/github/SpringCloudGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
